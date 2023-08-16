2b4dc2d2799922283c00009d3475f245e1100845
irykov <cerealwhay@gmail.com>
2023-08-17 02:33:22 +0300

    new bg test

--- src/components/ImageBg.vue
+++ src/components/ImageBg.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import {ref} from "vue";
 import bg from "@/assets/images/bg-test.jpg";
+import bg2 from "@/assets/images/bg2.jpeg";
 
-const bgLink = ref(bg)
+const bgLink = ref(bg2)
 </script>
 
 <template>
@@ -23,6 +24,7 @@ const bgLink = ref(bg)
     display: block;
     object-fit: cover;
     object-position: 0 0;
+    filter: brightness(75%);
   }
 }
 </style>
